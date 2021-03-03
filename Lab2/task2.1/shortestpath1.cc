7bbb64b28b36b17345e24a835d29b8fdf5bb076c
Zhongjun Ni <zhongjun.ni@liu.se>
2021-03-03 11:38:32 +0100

    Update task2.1

--- Lab2/task2.1/shortestpath1.cc
+++ Lab2/task2.1/shortestpath1.cc
@@ -1,128 +1,88 @@
+#include <algorithm>
 #include <climits>
+#include <cmath>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
 #include <queue>
 #include <set>
+#include <string>
+#include <utility>
+#include <vector>
 
 namespace aaps {
 namespace zhoni04 {
 
-using WeightType = long long;
-
-/**
- * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @class Edge
- * @brief Implements the edge, which consists of source node, destination node,
- * and the weight.
- */
-struct Edge {
- public:
-  /**
-   * @brief Initializes a new instance of edge.
-   * @param s: The source node.
-   * @param d: The destination node.
-   * @param w: The weight.
-   */
-  Edge(int s, int d, WeightType w) {
-    src = s;
-    dst = d;
-    weight = w;
-  }
-
-  int src;
-  int dst;
-  WeightType weight;
-};
-
-using GraphType = std::vector<std::vector<Edge>>;
-
-const WeightType kInfinity = LLONG_MAX;
+const int kInfinity = 100000000;
 const int kInvalidNode = -1;
 
 /**
  * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @brief Gets the next node from candidate nodes, which has current shortest
- * distance.
- * @param distance: The distance, which stores current shortest distance of
- * between start node and other node.
- * @param candidate_nodes: The candidate nodes.
- * @return The node, which has current shortest distance. If not found, return
- * kInvalidNode.
+ * @class Edge
+ * @brief Implements the edge, which consists of 'from' node, 'to' node, and the
+ * weight.
  */
-int GetNextNode(const std::vector<WeightType>& distance,
-                const std::set<int>& candidate_nodes) {
-  WeightType min_weight = LLONG_MAX;
-  int min_node = kInvalidNode;
+struct Edge {
+  int from;
+  int to;
+  int weight;
+};
 
-  for (auto itr = candidate_nodes.begin(); itr != candidate_nodes.end();
-       ++itr) {
-    if (distance[*itr] < min_weight) {
-      min_weight = distance[*itr];
-      min_node = *itr;
-    }
-  }
-
-  return min_node;
-}
+using GraphType = std::vector<std::vector<Edge>>;
+using ResultType = std::pair<std::vector<int>, std::vector<int>>;
 
 /**
  * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @brief Implement Dijkstras algorithm to find the shortest path from one node
- * to all other nodes in a graph with non-negative edge weights.
+ * @brief Implement Dijkstras algorithm to find the shortest path from one
+ * node to all other nodes in a graph with non-negative edge weights.
  * @param graph: The graph, which stores edges of each node.
  * @param start: The start node.
- * @param distance: The distance, used for storing the shortest distance of
- * between start node and other node.
- * @param parent: The parent, used for storing the parent node of each node.
+ * @return: A pair, the first is the distance vector and the second is the
+ * parent vector.
  */
-void ShortestPath(const GraphType& graph, int start,
-                  std::vector<WeightType>* distance,
-                  std::vector<int>* parent = nullptr) {
-  if (distance == nullptr) {
-    return;
-  }
-
+ResultType ShortestPath(const GraphType& graph, int start) {
   int n = graph.size();
-  distance->resize(n);
-  distance->assign(n, kInfinity);
-  if (parent != nullptr) {
-    parent->resize(n);
-    parent->assign(n, -1);
-  }
 
-  std::vector<bool> marked(n, false);
-  std::set<int> candidate_nodes;
-  (*distance)[start] = 0;
-  candidate_nodes.insert(start);
-  if (parent != nullptr) {
-    (*parent)[start] = start;
-  }
+  std::vector<int> distance(n, kInfinity);
+  std::vector<int> parent(n, kInvalidNode);
+  std::vector<bool> found(n, false);
 
-  while (!candidate_nodes.empty()) {
-    int curr_node = GetNextNode(*distance, candidate_nodes);
-    if (curr_node == kInvalidNode || (*distance)[curr_node] == kInfinity) {
-      break;
+  using DistanceNodePair = std::pair<int, int>;
+  using MinHeap =
+      std::priority_queue<DistanceNodePair, std::vector<DistanceNodePair>,
+                          std::greater<DistanceNodePair>>;
+
+  MinHeap heap;
+  heap.push(std::make_pair(0, start));
+  distance[start] = 0;
+  parent[start] = start;
+
+  while (!heap.empty()) {
+    auto p = heap.top();
+    heap.pop();
+
+    int d = p.first;
+    int u = p.second;
+
+    if (d > distance[u]) {
+      continue;
     }
 
-    marked[curr_node] = true;
-    candidate_nodes.erase(curr_node);
+    found[u] = true;
 
-    const std::vector<Edge>& edges = graph[curr_node];
-    WeightType curr_weight = (*distance)[curr_node];
-    int m = edges.size();
-    for (int i = 0; i < m; ++i) {
-      if (curr_weight + edges[i].weight < (*distance)[edges[i].dst]) {
-        (*distance)[edges[i].dst] = curr_weight + edges[i].weight;
+    for (auto& e : graph[u]) {
+      if (!found[e.to] && distance[e.to] > d + e.weight) {
+        distance[e.to] = d + e.weight;
+        parent[e.to] = u;
 
-        if (parent != nullptr) {
-          (*parent)[edges[i].dst] = curr_node;
-        }
-      }
-
-      if (!marked[edges[i].dst]) {
-        candidate_nodes.insert(edges[i].dst);
+        heap.push(std::make_pair(distance[e.to], e.to));
       }
     }
   }
+
+  return std::make_pair(distance, parent);
 }
 
 }  // namespace zhoni04
@@ -133,6 +93,7 @@ using namespace aaps::zhoni04;
 
 int main(void) {
   int n, m, q, s;
+  Edge e;
 
   while (scanf("%d %d %d %d", &n, &m, &q, &s) != EOF) {
     if (n == 0 && m == 0 && q == 0 && s == 0) {
@@ -141,22 +102,20 @@ int main(void) {
 
     GraphType graph(n);
 
-    int src, dst;
-    WeightType weight;
     for (int i = 0; i < m; ++i) {
-      scanf("%d %d %lld", &src, &dst, &weight);
-      graph[src].push_back(Edge(src, dst, weight));
+      scanf("%d %d %d", &e.from, &e.to, &e.weight);
+      graph[e.from].push_back(e);
     }
 
-    std::vector<WeightType> distance(n);
-    ShortestPath(graph, s, &distance);
+    auto result = ShortestPath(graph, s);
+    auto& distance = result.first;
 
     int qn;
     for (int i = 0; i < q; ++i) {
       scanf("%d", &qn);
 
       if (distance[qn] != kInfinity) {
-        printf("%lld\n", distance[qn]);
+        printf("%d\n", distance[qn]);
       } else {
         printf("Impossible\n");
       }
