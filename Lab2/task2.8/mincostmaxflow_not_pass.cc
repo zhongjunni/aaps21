eb367fc0e8bd63649cc78e338311915dd19b63ab
Zhongjun Ni <zhongjun.ni@liu.se>
2021-03-03 18:20:00 +0100

    Update task2.8

--- Lab2/task2.8/mincostmaxflow_not_pass.cc
+++ Lab2/task2.8/mincostmaxflow_not_pass.cc
@@ -1,71 +1,95 @@
 #include <algorithm>
 #include <climits>
+#include <cmath>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <map>
 #include <queue>
+#include <set>
+#include <string>
 #include <utility>
 #include <vector>
 
 namespace aaps {
 namespace zhoni04 {
 
-const int kInfinity = 100000001;
+using WeightType = long long;
+
+const WeightType kInfinity = 1000000001;
+const int kInvalidNode = -1;
 
 /**
  * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @brief Finds a path, which has the lowest cost, from vertex s to vertex t.
- * @param graph: The graph, graph[i][j] means there is a vertex from i to
- * graph[i][j].
+ * @class Edge
+ * @brief Implements the edge, which consists of 'from' node, 'to' node, the
+ * capacity, the flow, the cost, and 'rev' to help find the reverse edge.
+ */
+struct Edge {
+  Edge(int _from, int _to, WeightType _capacity, WeightType _flow,
+       WeightType _cost, int _rev)
+      : from(_from),
+        to(_to),
+        capacity(_capacity),
+        flow(_flow),
+        cost(_cost),
+        rev(_rev) {}
+
+  int from;
+  int to;
+  WeightType capacity;
+  WeightType flow;
+  WeightType cost;
+  int rev;
+};
+
+using GraphType = std::vector<std::vector<Edge>>;
+using ResultType = std::pair<std::pair<WeightType, WeightType>, GraphType>;
+
+/**
+ * @author Zhongjun Ni (LiU-ID: zhoni04)
+ * @brief Finds a path from vertex s to vertex t by BFS.
+ * @param graph: The graph.
  * @param s: The source vertex.
  * @param t: The target (sink) vertex.
- * @param cost: The cost, cost[u][v] stores the cost for edge u -> v.
- * @param capacity: The capacity, capacity[u][v] stores the capacity for edge u
- * -> v.
- * @param parent: Used for store found route.
- * @param potential: Used for store potential of each vertex.
+ * @param parent: For storing the edge between a vertex and its parent vertex.
  * @return: True if found a path, otherwise false.
  */
-bool ShortestPath(const std::vector<std::vector<int>>& graph, int s, int t,
-                  const std::vector<std::vector<int>>& cost,
-                  const std::vector<std::vector<int>>& capacity,
-                  std::vector<int>* parent, std::vector<int>* potential) {
+bool ShortestPath(GraphType& graph, int s, int t, std::vector<Edge*>* parent) {
   int n = graph.size();
-  std::vector<int> distance(n, kInfinity);
+  parent->assign(n, nullptr);
 
-  using Pair = std::pair<int, int>;
-  std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> unvisited;
-  unvisited.push(Pair(0, s));
-  (*parent)[s] = -1;
+  std::vector<WeightType> distance(n, kInfinity);
+  std::vector<bool> found(n, false);
+
+  using DistanceNodePair = std::pair<int, int>;
+  using MinHeap =
+      std::priority_queue<DistanceNodePair, std::vector<DistanceNodePair>,
+                          std::greater<DistanceNodePair>>;
+
+  MinHeap heap;
+  heap.push(std::make_pair(0, s));
   distance[s] = 0;
 
-  while (!unvisited.empty()) {
-    Pair p = unvisited.top();
-    unvisited.pop();
+  while (!heap.empty()) {
+    auto p = heap.top();
+    heap.pop();
 
+    int d = p.first;
     int u = p.second;
-    if (distance[p.second] < p.first) {
+
+    if (d > distance[u]) {
       continue;
     }
 
-    for (int i = 0; i < graph[u].size(); ++i) {
-      int v = graph[u][i];
-      if (capacity[u][v] > 0 && distance[v] > distance[u] + cost[u][v] +
-                                                  (*potential)[u] -
-                                                  (*potential)[v]) {
-        (*parent)[v] = u;
-        distance[v] =
-            distance[u] + cost[u][v] + (*potential)[u] - (*potential)[v];
+    found[u] = true;
 
-        unvisited.push(Pair(distance[v], v));
-      }
-    }
-  }
+    for (auto& e : graph[u]) {
+      //
 
-  if (distance[t] != kInfinity) {
-    for (int i = 0; i < n; ++i) {
-      (*potential)[i] += distance[i];
+      heap.push(std::make_pair(distance[e.to], e.to));
     }
-    return true;
   }
 
   return false;
@@ -73,103 +97,61 @@ bool ShortestPath(const std::vector<std::vector<int>>& graph, int s, int t,
 
 /**
  * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @brief Updates the capacity and flow of each involved edges by given route.
- * @param parent: The route.
+ * @brief Implements an algorithm for finding the maximum flow with the minimal
+ * cost in a graph. This is a generalization of maximum flow where each edge
+ * both has a capacity and a cost. The cost of a flow through an edge is the
+ * flow multiplied by the cost for that edge.
+ * @param graph: The graph.
  * @param s: The source vertex.
  * @param t: The target (sink) vertex.
- * @param capacity: The capacity, capacity[u][v] stores the capacity for edge u
- * -> v.
- * @param flow: The flow, flow[u][v] stores the flow for edge u -> v.
- * @return: Current added flow.
+ * @return: A pair. The first is the max flow. The second is the flow graph.
  */
-int Update(const std::vector<int>& parent, int s, int t,
-           std::vector<std::vector<int>>* capacity,
-           std::vector<std::vector<int>>* flow) {
-  int this_flow = INT_MAX;
-  int v = t;
-  int u;
-  while (v != s) {
-    u = parent[v];
-    this_flow = std::min(this_flow, (*capacity)[u][v]);
-    v = u;
-  }
-
-  v = t;
-  while (v != s) {
-    u = parent[v];
-
-    (*capacity)[u][v] -= this_flow;
-    (*capacity)[v][u] += this_flow;
-
-    (*flow)[u][v] += this_flow;
-    (*flow)[v][u] -= this_flow;
-
-    v = u;
-  }
-
-  return this_flow;
-}
-
-/**
- * @author Zhongjun Ni (LiU-ID: zhoni04)
- * @brief Implements a function that finds the maximum flow with a minmum cost
- * in a flow graph.
- * @param graph: The graph, graph[i][j] means there is a vertex from i to
- * graph[i][j].
- * @param s: The source vertex.
- * @param t: The target (sink) vertex.
- * @param cost: The cost, cost[u][v] stores the cost for edge u -> v.
- * @param capacity: The capacity, capacity[u][v] stores the capacity for edge u
- * -> v.
- * @param flow: The flow, flow[u][v] stores the flow for edge u -> v.
- * @return: A pair of value, the first value is the maximum flow and the second
- * value is minimum cost.
- */
-std::pair<int, int> MaxFlow(const std::vector<std::vector<int>>& graph, int s,
-                            int t, const std::vector<std::vector<int>>& cost,
-                            std::vector<std::vector<int>>* capacity,
-                            std::vector<std::vector<int>>* flow) {
+ResultType MaxFlowMinCost(const GraphType& graph, int s, int t) {
   int n = graph.size();
-  std::vector<int> parent(n, -1);
-  std::vector<int> potential(n, 0);
-  std::pair<int, int> maxflow_mincost(0, 0);
+  std::vector<Edge*> parent(n);
 
-  while (ShortestPath(graph, s, t, cost, *capacity, &parent, &potential)) {
-    int this_flow = Update(parent, s, t, capacity, flow);
-    maxflow_mincost.first += this_flow;
-    maxflow_mincost.second += this_flow * potential[t];
+  WeightType max_flow = 0;
+  WeightType min_cost = 0;
+  GraphType flow_graph(graph);
+
+  while (ShortestPath(flow_graph, s, t, &parent)) {
+    WeightType path_flow = kInfinity;
+    for (auto e = parent[t]; e != nullptr; e = parent[e->from]) {
+      path_flow = std::min(path_flow, e->capacity - e->flow);
+    }
+
+    max_flow += path_flow;
+
+    for (auto e = parent[t]; e != nullptr; e = parent[e->from]) {
+      e->flow += path_flow;
+      flow_graph[e->to][e->rev].flow -= path_flow;
+    }
   }
 
-  return maxflow_mincost;
+  return std::make_pair(max_flow, flow_graph);
 }
 
 }  // namespace zhoni04
 }  // namespace aaps
 
 using namespace aaps::zhoni04;
+using namespace std;
 
 int main(void) {
   int n, m, s, t;
   scanf("%d %d %d %d", &n, &m, &s, &t);
 
-  std::vector<std::vector<int>> graph(n);
-  std::vector<std::vector<int>> capacity(n, std::vector<int>(n, 0));
-  std::vector<std::vector<int>> cost(n, std::vector<int>(n, 0));
-  std::vector<std::vector<int>> flow(n, std::vector<int>(n, 0));
+  GraphType graph(n);
 
   int u, v, c, w;
   for (int i = 0; i < m; ++i) {
     scanf("%d %d %d %d", &u, &v, &c, &w);
-    graph[u].push_back(v);
-    graph[v].push_back(u);
-
-    capacity[u][v] = c;
-    cost[u][v] = w;
-    cost[v][u] = -w;
+    graph[u].emplace_back(u, v, c, 0, w, graph[v].size());
+    graph[v].emplace_back(v, u, 0, 0, -w, graph[u].size() - 1);
   }
 
-  std::pair<int, int> maxflow_mincost =
-      MaxFlow(graph, s, t, cost, &capacity, &flow);
+  auto result = MaxFlowMinCost(graph, s, t);
+  auto& maxflow_mincost = result.first;
 
   printf("%d %d\n", maxflow_mincost.first, maxflow_mincost.second);
 
