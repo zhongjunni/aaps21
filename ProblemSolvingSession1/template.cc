2c8fa5306383d34fe5b28a076ce9ab6684fd461f
Zhongjun Ni <zhongjun.ni@liu.se>
2021-03-04 09:50:57 +0100

    Update template

--- ProblemSolvingSession1/template.cc
+++ ProblemSolvingSession1/template.cc
@@ -1,11 +1,25 @@
 #include <algorithm>
+#include <climits>
 #include <cmath>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <map>
+#include <queue>
 #include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 
+namespace aaps {
+namespace zhoni04 {}
+}  // namespace aaps
+
+using namespace aaps::zhoni04;
 using namespace std;
 
 int main(void) { return 0; }
