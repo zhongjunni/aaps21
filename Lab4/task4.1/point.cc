fb044abb4ad47aaaf9a3e40fe9694c163d3d23d7
Zhongjun Ni <jcxcnzj@gmail.com>
2021-04-26 17:59:16 +0200

    Add init commit for lab4

---
+++ Lab4/task4.1/point.cc
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+namespace aaps {
+namespace zhoni04 {}
+}  // namespace aaps
