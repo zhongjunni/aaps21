6d2b2b7d85b1c194b6be25827473b37679f4ae78
Zhongjun Ni <jcxcnzj@gmail.com>
2021-05-12 21:57:07 +0200

    Add init commit for task4.10

---
+++ Lab4/task4.10/linearrecurrence.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+namespace aaps {
+namespace zhoni04 {}
+}  // namespace aaps
+
+using namespace aaps::zhoni04;
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  return 0;
+}
