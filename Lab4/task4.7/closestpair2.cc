7d20d465f364690738c376de54e16aaed6e531c0
Zhongjun Ni <jcxcnzj@gmail.com>
2021-05-13 23:46:39 +0200

    Add task4.7

--- Lab4/task4.7/closestpair2.cc
+++ Lab4/task4.7/closestpair2.cc
@@ -308,6 +308,55 @@ std::pair<double, std::vector<int>> DivideAndConquer(
   if (points_sort_by_x.size() <= 3) {
     return BruteForce(points_sort_by_x);
   }
+
+  int n = points_sort_by_x.size();
+  std::vector<PointWrapper> x_left(n / 2);
+  std::vector<PointWrapper> x_right(n - n / 2);
+
+  std::copy(points_sort_by_x.begin(), points_sort_by_x.begin() + n / 2,
+            x_left.begin());
+  std::copy(points_sort_by_x.begin() + n / 2, points_sort_by_x.end(),
+            x_right.begin());
+
+  double mid_x = points_sort_by_x[n / 2].x();
+
+  std::vector<PointWrapper> y_left;
+  std::vector<PointWrapper> y_right;
+  for (auto& p : points_sort_by_y) {
+    if (p.x() <= mid_x) {
+      y_left.push_back(p);
+    } else {
+      y_right.push_back(p);
+    }
+  }
+
+  auto pair1 = DivideAndConquer(x_left, y_left);
+  auto pair2 = DivideAndConquer(x_right, y_right);
+  auto min_pair = pair1;
+  if (pair2.first < pair1.first) {
+    min_pair = pair2;
+  }
+
+  std::vector<PointWrapper> stripe;
+  for (auto& p : points_sort_by_y) {
+    if (std::fabs(p.x() - mid_x) < min_pair.first) {
+      stripe.push_back(p);
+    }
+  }
+
+  for (int i = 0; i < stripe.size(); ++i) {
+    for (int j = i + 1;
+         j < stripe.size() && stripe[j].y() - stripe[i].y() < min_pair.first;
+         ++j) {
+      double dist = (stripe[j] - stripe[i]).Length();
+      if (dist < min_pair.first) {
+        min_pair.first = dist;
+        min_pair.second[0] = stripe[i].index();
+        min_pair.second[1] = stripe[j].index();
+      }
+    }
+  }
+  return min_pair;
 }
 
 /**
@@ -318,7 +367,16 @@ std::pair<double, std::vector<int>> DivideAndConquer(
  * @param points: A set of points.
  * @return: The indexes for a pair of points with minimal distance between them.
  */
-std::vector<int> ClosestPair(const std::vector<PointWrapper>& points) {}
+std::vector<int> ClosestPair(const std::vector<PointWrapper>& points) {
+  std::vector<PointWrapper> points_sort_by_x(points);
+  std::sort(points_sort_by_x.begin(), points_sort_by_x.end());
+
+  std::vector<PointWrapper> points_sort_by_y(points);
+  std::sort(points_sort_by_y.begin(), points_sort_by_y.end(), CompareY);
+
+  auto result = DivideAndConquer(points_sort_by_x, points_sort_by_y);
+  return result.second;
+}
 
 }  // namespace zhoni04
 }  // namespace aaps
